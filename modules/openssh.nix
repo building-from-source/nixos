b5493a493291c5fadca0d3aa15411c8612673edd
MayNiklas <info@niklas-steffen.de>
2021-03-06 16:15:09 +0100

    removed unused modules

--- modules/openssh.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }: {
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    startWhenNeeded = true;
-    challengeResponseAuthentication = false;
-  };
-}
