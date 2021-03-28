29b35d221003afb5dab70fe76ed75324162078cb
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-03-28 11:59:09 +0200

    Create module nix-common -> use binary cache

---
+++ modules/nix-common.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }: {
+
+  nixpkgs = { config.allowUnfree = true; };
+
+  nix = {
+
+    binaryCachePublicKeys = ["cache.lounge.rocks:uXa8UuAEQoKFtU8Om/hq6d7U+HgcrduTVr8Cfl6JuaY="];
+    binaryCaches = [ "https://cache.lounge.rocks" ];
+    trustedBinaryCaches =  ["https://cache.lounge.rocks"];
+
+    # Save space by hardlinking store files
+    autoOptimiseStore = true;
+
+    # Clean up old generations after 30 days
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    # Users allowed to run nix
+    allowedUsers = [ "root" ];
+  };
+}
