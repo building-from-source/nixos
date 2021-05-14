99eb0c3814eceba17c1f9bd35d63654e5b93745a
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-05-14 19:15:43 +0200

    Update MayNiklas modules being used

--- machines/thinkpad-x250/configuration.nix
+++ machines/thinkpad-x250/configuration.nix
@@ -5,9 +5,9 @@
 
 let
   mayniklas = builtins.fetchGit {
-    # Updated 2020-03-08
+    # Updated 2020-05-14
     url = "https://github.com/mayniklas/nixos";
-    rev = "b41eb613ac628302e09ef8e04882fc0175472b57";
+    rev = "d9600cce022f6c91506f0d5e965437482ea16ddb";
   };
 in {
   imports = [
@@ -19,42 +19,44 @@ in {
     ../../users/julian.nix
 
     # Modules imported from MayNiklas
-    "${mayniklas}/modules/bluetooth.nix"
-    "${mayniklas}/modules/grub-luks.nix"
-    "${mayniklas}/modules/locale.nix"
-    "${mayniklas}/modules/networking.nix"
-    "${mayniklas}/modules/openssh.nix"
-    "${mayniklas}/modules/options.nix"
-    "${mayniklas}/modules/sound.nix"
-    "${mayniklas}/modules/yubikey.nix"
-    "${mayniklas}/modules/kde.nix"
+    "${mayniklas}/modules/bluetooth"
+    "${mayniklas}/modules/grub-luks"
+    "${mayniklas}/modules/locale"
+    "${mayniklas}/modules/networking"
+    "${mayniklas}/modules/openssh"
+    "${mayniklas}/modules/options"
+    "${mayniklas}/modules/sound"
+    "${mayniklas}/modules/kde"
+    "${mayniklas}/modules/yubikey"
+    "${mayniklas}/modules/zsh"
 
     # Modules
     ../../modules/nix-common.nix
-    ../../modules/zsh.nix
 
   ];
 
-  mainUser = "julian";
-  mainUserHome = "${config.users.extraUsers.${config.mainUser}.home}";
-
   mayniklas = {
+    var.mainUser = "julian";
+    bluetooth.enable = true;
     grub-luks = {
       enable = true;
       uuid = "2fcb1d42-da4a-472f-b13c-8ef727389806";
     };
+    kde.enable = true;
+    locale.enable = true;
+    networking.enable = true;
+    openssh.enable = true;
+    sound.enable = true;
+    yubikey.enable = true;
+    zsh.enable = true;
+
   };
 
   networking = { hostName = "thinkpad-x250"; };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    bash-completion
-    git
-    nixfmt
-    wget
-  ];
+  environment.systemPackages = with pkgs; [ bash-completion git nixfmt wget ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
