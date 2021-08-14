f17968d99aa5c73664fc9ad56f4459fad9d745b8
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-08-14 14:58:55 +0200

    update deprecated htop setting

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -46,10 +46,12 @@
 
   programs.htop = {
     enable = true;
-    treeView = true;
-    showCpuFrequency = true;
-    showCpuUsage = true;
-    showProgramPath = false;
+    settings = {
+      show_cpu_frequency = true;
+      show_cpu_usage = true;
+      show_program_path = true;
+      tree_view = false;
+    };
   };
 
   programs.jq.enable = true;
